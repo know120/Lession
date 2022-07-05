341709e01329ae35c9fb6855418ef08a1b465180
know120 <knowsujay@gmail.com>
2022-07-05 19:47:25 +0530

    used list comprehension and loop

--- freecodecamp/dataAnalysisWithPython/mean_var_std.py
+++ freecodecamp/dataAnalysisWithPython/mean_var_std.py
@@ -11,44 +11,25 @@ def calculate(list):
     array = np.array(list)
     array = array.reshape(3, 3)
 
-    # calculate mean
-    axis1 = np.mean(array, axis=0).tolist()
-    axis2 = np.mean(array, axis=1).tolist()
-    flatten = np.mean(array, axis=None).tolist()
-    mean = [axis1, axis2, flatten]
+    # matrix axis types
+    matrix_type = [0, 1, None]
+    # list of operations
+    operation_names = ['mean', 'variance',
+                       'standard deviation', 'max', 'min', 'sum']
+    operations = [np.mean, np.var, np.std, np.max, np.min, np.sum]
 
-    # calculate variance,
-    axis1 = np.var(array, axis=0).tolist()
-    axis2 = np.var(array, axis=1).tolist()
-    flatten = np.var(array, axis=None)
-    variance = [axis1, axis2, flatten]
+    # list of operation results
+    calculations = {
+        operation_name : [
+            operation(array, axis=i).tolist() for i in matrix_type
+        ] for operation_name, operation in zip(operation_names, operations)
+    }
 
-    # calculate standard deviation,
-    axis1 = np.std(array, axis=0).tolist()
-    axis2 = np.std(array, axis=1).tolist()
-    flatten = np.std(array, axis=None)
-    standard_deviation = [axis1, axis2, flatten]
+    # # result dictionary
+    # calculations = {}
+    # # for each operation doing calculations
+    # for operation, result in zip(operations, operation_names):
+    #     calculations[result] = [operation(array, axis=i).tolist() for i in matrix_type]
 
-    # calculate max, min
-    axis1 = np.max(array, axis=0).tolist()
-    axis2 = np.max(array, axis=1).tolist()
-    flatten = np.max(array, axis=None)
-    max = [axis1, axis2, flatten]
-
-    axis1 = np.min(array, axis=0).tolist()
-    axis2 = np.min(array, axis=1).tolist()
-    flatten = np.min(array, axis=None)
-    min = [axis1, axis2, flatten]
-
-    # calculate sum of the rows, columns, and elements
-    axis1 = np.sum(array, axis=0).tolist()
-    axis2 = np.sum(array, axis=1).tolist()
-    flatten = np.sum(array, axis=None)
-    sum = [axis1, axis2, flatten]
-
-    # make a dictionary with the calculated values
-    calculations = {'mean': mean, 'variance': variance, 'standard deviation': standard_deviation,
-                    'max': max, 'min': min, 'sum': sum}
-    
-    # return dictionary
+    # return calculations dictionary
     return calculations
